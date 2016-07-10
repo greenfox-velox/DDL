fb2ab873edef1fceebf7a3c8aee15b7596b748b4
David <doczi.david.daci@gmail.com>
2016-07-10 22:00:30 +0200

    week8, yoda update

--- week-08/day6/yoda.js
+++ week-08/day6/yoda.js
@@ -2,23 +2,21 @@
 
 var yButton = document.querySelector('button');
 yButton.addEventListener('click', yodanize);
-var result = document.querySelector('p');
+var result = document.querySelector('h2');
 
 
 function yodanize() {
-  console.log('Hello');
   var xhr = new XMLHttpRequest();
   var input = document.querySelector('.input_text');
-
   var text = input.value;
   var url = 'https://yoda.p.mashape.com/yoda?sentence=';
   var urlResult = url + encodeURIComponent(text);
   xhr.open('GET', urlResult);
-  xhr.setRequestHeader("X-Mashape-Key",  "tic9M4Iz2FmshcI5RvLVTGVqAzAgp1MprXmjsnnfFJ0G0TqIFJ");
+  xhr.setRequestHeader("X-Mashape-Key", "tic9M4Iz2FmshcI5RvLVTGVqAzAgp1MprXmjsnnfFJ0G0TqIFJ");
   xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-          result.textContent = xhr.response
-      };
+      if (xhr.readyState === 4) {
+          result.textContent = xhr.response;
+      }
     };
   xhr.send();
 }
